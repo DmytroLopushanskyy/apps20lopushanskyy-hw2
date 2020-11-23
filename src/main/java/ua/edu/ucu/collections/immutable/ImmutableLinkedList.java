79ac6358e0a507f72cebfa3cb6259d650edb750f
DmytroLopushanskyy <dmytrolopushanskyy@gmail.com>
2020-11-23 23:17:07 +0200

    fixed jenkins errors

--- src/main/java/ua/edu/ucu/collections/immutable/ImmutableLinkedList.java
+++ src/main/java/ua/edu/ucu/collections/immutable/ImmutableLinkedList.java
@@ -3,47 +3,7 @@ package ua.edu.ucu.collections.immutable;
 import java.util.Arrays;
 
 
-class Node {
-    private Object value;
-    private Node prevNode;
-    private Node nextNode;
 
-    public Node(Object val) {
-        value = val;
-        prevNode = null;
-        nextNode = null;
-    }
-
-    public Node(Object val, Node prevVal, Node nextVal) {
-        value = val;
-        prevNode = prevVal;
-        nextNode = nextVal;
-    }
-
-    public void setValue(Object val) {
-        this.value = val;
-    }
-
-    public void setPrevNode(Node prevN) {
-        this.prevNode = prevN;
-    }
-
-    public void setNextNode(Node nextN) {
-        this.nextNode = nextN;
-    }
-
-    public Object getValue() {
-        return value;
-    }
-
-    public Node getPrevNode() {
-        return prevNode;
-    }
-
-    public Node getNextNode() {
-        return nextNode;
-    }
-}
 
 public class ImmutableLinkedList implements ImmutableList {
     private Node first;
@@ -113,7 +73,8 @@ public class ImmutableLinkedList implements ImmutableList {
     }
 
     private boolean addElementsAtPositionIfNeeded(int counter, int index,
-                                                  Node current, Object[] e) {
+                                                  Node curr, Object[] e) {
+        Node current = curr;
         if (counter == index) {
             Node newNode = new Node(e[0], current, null);  // Create new Node
             Node continueNode;
